c520e2bc0ebc711032e8d9869a01cfcde97f2ed2
Anton-front <trofimov.antonua85@gmail.com>
2019-11-16 21:03:38 +0200

    Updates

---
+++ precache-manifest.ec5aa7103f32a0ba5aa1b2ce9e1bfc55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62852633795c5eba5dcf6fd13a9c1ac5",
+    "url": "/anton-trofimov-12-11-2019/index.html"
+  },
+  {
+    "revision": "7ea8ce755d75d7656d4a",
+    "url": "/anton-trofimov-12-11-2019/static/css/main.074262d5.chunk.css"
+  },
+  {
+    "revision": "b1358931ce1252516180",
+    "url": "/anton-trofimov-12-11-2019/static/js/2.dcb71988.chunk.js"
+  },
+  {
+    "revision": "7ea8ce755d75d7656d4a",
+    "url": "/anton-trofimov-12-11-2019/static/js/main.54f6182b.chunk.js"
+  },
+  {
+    "revision": "732a8d007f6c113ba0e1",
+    "url": "/anton-trofimov-12-11-2019/static/js/runtime-main.f6b9ce5f.js"
+  },
+  {
+    "revision": "3d7b78de03796df2fe63f0b1d247cf21",
+    "url": "/anton-trofimov-12-11-2019/static/media/weather-ico.3d7b78de.png"
+  }
+]);
