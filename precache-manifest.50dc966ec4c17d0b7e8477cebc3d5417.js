14578c22817ca1aad98fdc9b1fbbe00f88ac14fc
Anton-front <trofimov.antonua85@gmail.com>
2019-11-16 20:54:04 +0200

    Updates

---
+++ precache-manifest.50dc966ec4c17d0b7e8477cebc3d5417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b5516b52ce4849e01c7cca2467ad10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59d8a048882429c76d72",
+    "url": "/static/css/main.074262d5.chunk.css"
+  },
+  {
+    "revision": "67b42a121c1f055657d9",
+    "url": "/static/js/2.5b53fd64.chunk.js"
+  },
+  {
+    "revision": "59d8a048882429c76d72",
+    "url": "/static/js/main.29bec8c3.chunk.js"
+  },
+  {
+    "revision": "00f08ea8a0845fa4b6aa",
+    "url": "/static/js/runtime-main.556f9d0d.js"
+  },
+  {
+    "revision": "3d7b78de03796df2fe63f0b1d247cf21",
+    "url": "/static/media/weather-ico.3d7b78de.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/anton-trofimov-12-11-2019/precache-manifest.eb06e03e06746d4ffb401320af3129b1.js"
+  "/precache-manifest.50dc966ec4c17d0b7e8477cebc3d5417.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/anton-trofimov-12-11-2019/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
